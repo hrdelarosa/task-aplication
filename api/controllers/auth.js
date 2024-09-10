68014bcba7e879ad044773d6e965a08c0132ee68
Hozmel Roberto De la Rosa Rendón <hozdelarosa@outlook.com>
2024-09-09 18:49:42 -0600

    Revert "Upload project to github"
    This reverts commit c4cc6dbb7a40a1da0d70e22970fdaf743dbf92e5.

--- api/controllers/auth.js
+++
@@ -1,9 +0,0 @@
-import { AuthModel } from '../models/database/auth.js'
-
-export class AuthController {
-  static async register (req, res) {
-    const user = await AuthModel.register({ input: req.body })
-
-    res.status(200).json(user)
-  }
-}
--- api/routes/auth.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { AuthController } from "../controllers/auth.js";
-
-export const authRouter = Router()
-
-authRouter.patch('/register', AuthController.register)
